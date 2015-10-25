3716b1fc18180437456e1c8e92b3dad61fff4503
jpouba <pouba@students.zcu.cz>
2015-10-25 23:10:48 +0100

    inout parsing

--- c_cmd.cpp
+++ c_cmd.cpp
@@ -4,6 +4,12 @@
 #define MAX_PARAM_LEN 300
 #define MAX_LINE_LEN 1000
 #define MAX_PATH_LEN 200
+#define MAX_ARGC 20
+
+#define STR_REDIRECT_OUT ">"
+#define STR_REDIRECT_IN "<"
+#define STR_REDIRECT_OUT_APP ">>"
+#define STR_REDIRECT_ERR "2>"
 
 #define CMD_END_CHAR 59
 #define CMD_END_CHAR_LINE 10
@@ -26,6 +32,9 @@ void c_run(LPTHREAD_START_ROUTINE f, run_params* params) {
 int parse_cmd(char* cmd, run_params* par);
 int parse_line(char* line, run_params* par);
 void get_path(node* node, char* path, int* position);
+
+int isTextPresent(char* src, int pos, char* target);
+
 
 void dir(run_params* par);
 void echo(run_params* par);
@@ -99,87 +108,161 @@ int parse_line(char* line, run_params* par) {
 
 int fill_string(char* source, int* from, char* dest) {
 	int p_pos = 0, pos = *from;
+	int in_quotes = 0;
+	int i;
 
-	while ((source[pos] != ' ') && (source[pos] != '\0')) {
+	while (source[pos] == ' ') pos++; // ignore leading spaces
+	if (source[pos] == '\0') return 1;
+
+	// check if text starts with any redirects
+	if (isTextPresent(source, pos, STR_REDIRECT_OUT_APP)) {
+		int len = strlen(STR_REDIRECT_OUT_APP);
+		for (i = 0; i < len; i++) dest[i] = source[pos + i];
+		dest[len] = '\0';
+		pos += len;
+		(*from) = pos;
+		return 0;
+	}
+	if (isTextPresent(source, pos, STR_REDIRECT_ERR)) {
+		int len = strlen(STR_REDIRECT_ERR);
+		for (i = 0; i < len; i++) dest[i] = source[pos + i];
+		dest[len] = '\0';
+		pos += len;
+		(*from) = pos;
+		return 0;
+	}
+	if (isTextPresent(source, pos, STR_REDIRECT_IN)) {
+		int len = strlen(STR_REDIRECT_IN);
+		for (i = 0; i < len; i++) dest[i] = source[pos + i];
+		dest[len] = '\0';
+		pos += len;
+		(*from) = pos;
+		return 0;
+	}
+	if (isTextPresent(source, pos, STR_REDIRECT_OUT)) {
+		int len = strlen(STR_REDIRECT_OUT);
+		for (i = 0; i < len; i++) dest[i] = source[pos + i];
+		dest[len] = '\0';
+		pos += len;
+		(*from) = pos;
+		return 0;
+	}
+
+	while (source[pos] != '\0') {
+
+		if (!in_quotes) {
+			if (isTextPresent(source, pos, STR_REDIRECT_IN)) break;
+			if (isTextPresent(source, pos, STR_REDIRECT_OUT)) break;
+			if (isTextPresent(source, pos, STR_REDIRECT_OUT_APP)) break;
+			if (isTextPresent(source, pos, STR_REDIRECT_ERR)) break;
+
+			if (source[pos] == ' ') break;
+			if (source[pos] == '"') {
+				in_quotes = 1;
+				pos++;
+				continue;
+			}
+		}
+		else {
+			if (source[pos] == '"') {
+				in_quotes = 0;
+				pos++;
+				continue;
+			}
+			if ((source[pos] == '\\') && source[pos+1] == '"') {
+				pos++;
+			}
+		}
+
 		dest[p_pos] = source[pos];
 		pos++;
 		p_pos++;
 	}
+
 	dest[p_pos] = '\0';
 	(*from) = pos;
 
 	return 0;
 }
 
+int isTextPresent(char* src, int pos, char* target) {
+	int len = strlen(target);
+	int i;
+
+	for (i = 0; i < len; i++) {
+		if (src[pos + i] != target[i]) return 0;
+	}
+
+	return 1;
+}
+
 int parse_cmd(char* cmd, run_params* par) {
-	printf("*** %s\n", cmd);
-	int pos, p_pos;
+	int pos, p_pos, i;
 
 	char* cmd_itself = (char*)malloc(sizeof(char) * MAX_CMD_LEN);
 	char* input = (char*)malloc(sizeof(char) * MAX_PATH_LEN);
 	char* output = (char*)malloc(sizeof(char) * MAX_PATH_LEN);
 	char* err_output = (char*)malloc(sizeof(char) * MAX_PATH_LEN);
+	int out_append = 0;
+	for (i = 0; i < MAX_PATH_LEN; i++) {
+		input[i] = '\0';
+		output[i] = '\0';
+		err_output[i] = '\0';
+	}
 
+	char** args = (char**)malloc(sizeof(char*) * MAX_ARGC);
 	char* arg = (char*)malloc(sizeof(char) * MAX_PARAM_LEN);
 	int argc = 0;
 
+
 	pos = 0;
-	while (cmd[pos] == ' ') pos++; // ignore leading spaces
+	int ret;
+	ret = fill_string(cmd, &pos, cmd_itself);
+	if (ret != 0) return 0; // empty command
 
-	fill_string(cmd, &pos, cmd_itself);
-	printf("cmd: %s\n", cmd_itself);
+	while (1) {
+		ret = fill_string(cmd, &pos, arg);
+		if (ret != 0) break;
 
-	// TODO
+		printf("** %s\n", arg);
 
-	while (cmd[pos] != '\0') {
-		// some parameters or redirects
-		while (cmd[pos] == ' ') pos++; // ignore leading spaces
-
-		if (cmd[pos] == '<') {
-			pos++;
-			while (cmd[pos] == ' ') pos++; // ignore leading spaces
-			if (cmd[pos] == '\0') {
-				printf("Error when parsing 'in'.\n");
-				return 2;
-			}
-			fill_string(cmd, &pos, input);
-			printf("input: %s\n", input);
+		if (strcmp(arg, STR_REDIRECT_IN) == 0) {
+			ret = fill_string(cmd, &pos, arg);
+			if (ret != 0) break;
+			strcpy_s(input, MAX_PATH_LEN, arg);
 		}
-		else if (cmd[pos] == '>') {
-			pos++;
-			while (cmd[pos] == ' ') pos++; // ignore leading spaces
-			if (cmd[pos] == '\0') {
-				printf("Error when parsing 'out'.\n");
-				return 2;
-			}
-			fill_string(cmd, &pos, output);
-			printf("output: %s\n", output);
+		else if (strcmp(arg, STR_REDIRECT_OUT) == 0) {
+			ret = fill_string(cmd, &pos, arg);
+			if (ret != 0) break;
+			out_append = 0;
+			strcpy_s(output, MAX_PATH_LEN, arg);
 		}
-		else if (cmd[pos] == '*') {
-			pos++;
-			while (cmd[pos] == ' ') pos++; // ignore leading spaces
-			if (cmd[pos] == '\0') {
-				printf("Error when parsing 'err'.\n");
-				return 2;
-			}
-			fill_string(cmd, &pos, err_output);
-			printf("err: %s\n", err_output);
+		else if (strcmp(arg, STR_REDIRECT_OUT_APP) == 0) {
+			ret = fill_string(cmd, &pos, arg);
+			if (ret != 0) break;
+			out_append = 1;
+			strcpy_s(output, MAX_PATH_LEN, arg);
+		}
+		else if (strcmp(arg, STR_REDIRECT_ERR) == 0) {
+			ret = fill_string(cmd, &pos, arg);
+			if (ret != 0) break;
+			strcpy_s(err_output, MAX_PATH_LEN, arg);
 		}
 		else {
-			while (cmd[pos] == ' ') pos++; // ignore leading spaces
-			if (cmd[pos] == '\0') {
-				printf("Error when parsing 'args'.\n");
-				return 2;
-			}
-			fill_string(cmd, &pos, arg);
+			args[argc] = arg;
 			argc++;
-			printf("%d. arg: %s\n", argc, arg);
+			arg = (char*)malloc(sizeof(char) * MAX_PARAM_LEN);
 		}
-
-		//pos++;
 	}
 
-
+	printf("CMD: %s\n", cmd_itself);
+	printf("Out [%s]: %s\n", out_append ? "YES" : "NO", output);
+	printf("In: %s\n", input);
+	printf("Err: %s\n", err_output);
+	printf("Argc: %d\n", argc);
+	for (i = 0; i < argc; i++) {
+		printf(" %d. arg: %s\n", i, args[i]);
+	}
 
 
 
